4cbdc41a11865e6788eaffdd893d7679ee660682
rockerBoy <antonpedan01@gmail.com>
2020-07-28 23:44:03 +0300

    Created product-form.js for admin FE part Basic view modification Added symfony/dependency-injection component

--- src/views/admin-page-product-export.php
+++ src/views/admin-page-product-export.php
@@ -15,7 +15,7 @@ wp_enqueue_script('wc-product-export');
             <form class="woocommerce-exporter">
                 <header>
                     <span class="spinner is-active"></span>
-                    <h2><?php esc_html_e('Export products to a CSV file', 'woocommerce'); ?></h2>
+                    <h2><?php esc_html_e('Экспорт товаров в Excel файл', 'extended-woo'); ?></h2>
                     <p><?php esc_html_e('This tool allows you to generate and download a CSV file containing a list of all products.', 'woocommerce'); ?></p>
                 </header>
                 <section>
@@ -26,67 +26,25 @@ wp_enqueue_script('wc-product-export');
                                 <label for="woocommerce-exporter-columns"><?php esc_html_e('Which columns should be exported?', 'woocommerce'); ?></label>
                             </th>
                             <td>
-                                <select id="woocommerce-exporter-columns" class="woocommerce-exporter-columns wc-enhanced-select" style="width:100%;" multiple data-placeholder="<?php esc_attr_e('Export all columns', 'woocommerce'); ?>">
-                                    <?php
-//                                    foreach ($exporter->get_default_column_names() as $column_id => $column_name) {
-//                                        echo '<option value="' . esc_attr($column_id) . '">' . esc_html($column_name) . '</option>';
-//                                    }
-                                    ?>
-                                    <option value="downloads"><?php esc_html_e('Downloads', 'woocommerce'); ?></option>
-                                    <option value="attributes"><?php esc_html_e('Attributes', 'woocommerce'); ?></option>
-                                </select>
+                                test
                             </td>
                         </tr>
-    <!--                    <tr>-->
-    <!--                        <th scope="row">-->
-    <!--                            <label for="woocommerce-exporter-types">--><?php //esc_html_e('Which product types should be exported?', 'woocommerce'); ?><!--</label>-->
-    <!--                        </th>-->
-    <!--                        <td>-->
-    <!--                            <select id="woocommerce-exporter-types" class="woocommerce-exporter-types wc-enhanced-select" style="width:100%;" multiple data-placeholder="--><?php //esc_attr_e('Export all products', 'woocommerce'); ?><!--">-->
-    <!--                                --><?php
-    //                                foreach (wc_get_product_types() as $value => $label) {
-    //                                    echo '<option value="' . esc_attr($value) . '">' . esc_html($label) . '</option>';
-    //                                }
-    //                                ?>
-    <!--                                <option value="variation">--><?php //esc_html_e('Product variations', 'woocommerce'); ?><!--</option>-->
-    <!--                            </select>-->
-    <!--                        </td>-->
-    <!--                    </tr>-->
-    <!--                    <tr>-->
-    <!--                        <th scope="row">-->
-    <!--                            <label for="woocommerce-exporter-category">--><?php //esc_html_e('Which product category should be exported?', 'woocommerce'); ?><!--</label>-->
-    <!--                        </th>-->
-    <!--                        <td>-->
-    <!--                            <select id="woocommerce-exporter-category" class="woocommerce-exporter-category wc-enhanced-select" style="width:100%;" multiple data-placeholder="--><?php //esc_attr_e('Export all categories', 'woocommerce'); ?><!--">-->
-    <!--                                --><?php
-    //                                $categories = get_categories(
-    //                                    array(
-    //                                        'taxonomy'   => 'product_cat',
-    //                                        'hide_empty' => false,
-    //                                    )
-    //                                );
-    //                                foreach ($categories as $category) {
-    //                                    echo '<option value="' . esc_attr($category->slug) . '">' . esc_html($category->name) . '</option>';
-    //                                }
-    //                                ?>
-    <!--                        </td>-->
-    <!--                    </tr>-->
-    <!--                    <tr>-->
-    <!--                        <th scope="row">-->
-    <!--                            <label for="woocommerce-exporter-meta">--><?php //esc_html_e('Export custom meta?', 'woocommerce'); ?><!--</label>-->
-    <!--                        </th>-->
-    <!--                        <td>-->
-    <!--                            <input type="checkbox" id="woocommerce-exporter-meta" value="1" />-->
-    <!--                            <label for="woocommerce-exporter-meta">--><?php //esc_html_e('Yes, export all custom meta', 'woocommerce'); ?><!--</label>-->
-    <!--                        </td>-->
-                           </tr>
+                        <tr>
+                            <th scope="row">
+                                <label for="woocommerce-exporter-meta"><?php esc_html_e( 'Export custom meta?', 'woocommerce' ); ?></label>
+                            </th>
+                            <td>
+                                <input type="checkbox" id="woocommerce-exporter-meta" value="1" />
+                                <label for="woocommerce-exporter-meta"><?php esc_html_e( 'Yes, export all custom meta', 'woocommerce' ); ?></label>
+                            </td>
+                        </tr>
                         <?php do_action('woocommerce_product_export_row'); ?>
                         </tbody>
                     </table>
                     <progress class="woocommerce-exporter-progress" max="100" value="0"></progress>
                 </section>
                 <div class="wc-actions">
-                    <button type="submit" class="woocommerce-exporter-button button button-primary" value="<?php esc_attr_e('Generate CSV', 'woocommerce'); ?>"><?php esc_html_e('Generate CSV', 'woocommerce'); ?></button>
+                    <button type="submit" class="woocommerce-exporter-button button button-primary" value="<?php esc_attr_e('Сгенерировать Эксель', 'extended-woo'); ?>"><?php esc_html_e('Сгенерировать Эксель', 'extended-woo'); ?></button>
                 </div>
             </form>
         </div>
